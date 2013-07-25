68bd15cf4e910097acf307292c37815ade57c4e3
Una <Una21136@yahoo.com>
2013-07-25 00:41:07 -0400

    cleaned up some things, added a ReadMe with content

--- colourify.js
+++ colourify.js
@@ -1,43 +1,43 @@
 (function(window, $) {
-    var Colourify = function(elem, options) {
-        this.elem = elem;
-        this.$elem = $(elem);
-        this.options = options;
-        this.metadata = this.$elem.data( 'plugin-options' );
-    };
+	var Colourify = function(elem, options) {
+		this.elem = elem;
+		this.$elem = $(elem);
+		this.options = options;
+		this.metadata = this.$elem.data( 'plugin-options' );
+	};
 
-    Colourify.prototype = {
-        defaults: {
-            colors: ["#17A39E","#F26D83","#67AF45","#F7941E"],
-            property: "color"
-        },
-        init: function() {
-            // this.config is how you can access the defaults 
-            this.config = $.extend({}, this.defaults, this.options, this.metadata);
+	Colourify.prototype = {
+		// the defaults are unakravets.com colors, editing the 'color' property
+		defaults: {
+			colors: ["#17a39e","#f26d83","#67af45","#f7941e"],
+			property: "color"
+		},
+		init: function() {
+			this.config = $.extend({}, this.defaults, this.options, this.metadata);
 
-            //set global variables here
-            this.chosenColor = this.chooseColor();
+			this.chosenColor = this.chooseColor();
 
-            //refer to your methods here
-            this.colorIt();
-            return this;
-        },
+			this.colorIt();
+			return this;
+		},
 
-        chooseColor: function() {
-            randno = Math.floor(Math.random() * this.config.colors.length); 
-            chosenColor = this.config.colors[randno];
-        },
+		chooseColor: function() {
+			// chooses a random color from the list you provide (or default)
+			randno = Math.floor(Math.random() * this.config.colors.length); 
+			chosenColor = this.config.colors[randno];
+		},
 
-        colorIt: function() { 
-            this.$elem.css(this.config.property, chosenColor);
-        }
-    };
+		colorIt: function() { 
+			//changes the property you provide (or color property) to the randomly chosen color
+			this.$elem.css(this.config.property, chosenColor);
+		}
+	};
 
-    $.fn.colourify = function(options) {
-        return this.each(function(){
-            new Colourify(this, options).init();
-        });
-    };
+	$.fn.colourify = function(options) {
+		return this.each(function(){
+			new Colourify(this, options).init();
+		});
+	};
 
-    window.Colourify = Colourify;
+	window.Colourify = Colourify;
 })(window, jQuery); 
